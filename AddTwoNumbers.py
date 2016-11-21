135ac15f141417e56b0bec49c5824740839df3d6
Mars Sun <marsunique@gmail.com>
2016-11-21 00:06:30 -0500

    next permutation, add two linklist, implement strStr()

---
+++ AddTwoNumbers.py
@@ -0,0 +1,68 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        head = n = ListNode(-1)
+        carry = 0
+        while l1 or l2 or carry:
+            sum = carry
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+            carry = sum / 10
+            node = ListNode(sum % 10)
+            n.next = node
+            n = n.next
+        return head.next
+
+# in-place, no extra link list
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        head = ListNode(-1)
+        head.next = l1
+        pre = head
+        carry = 0
+        while l1 and l2:
+            sum = l1.val + l2.val + carry
+            l1.val = sum % 10
+            carry = sum / 10
+            pre = l1
+            l1 = l1.next
+            l2 = l2.next
+        while l1:
+            sum = l1.val + carry
+            l1.val = sum % 10
+            carry = sum / 10
+            if not carry:
+                break
+            pre = l1
+            l1 = l1.next
+        while l2:
+            pre.next = l2
+            sum = l2.val + carry
+            l2.val = sum % 10
+            carry = sum / 10
+            if not carry:
+                break
+            pre = pre.next
+            l2 = l2.next
+        if carry == 1:
+            node = ListNode(1)
+            pre.next = node
+        return head.next
