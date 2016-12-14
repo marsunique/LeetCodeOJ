4801adcd10a894db2eb6b40466b396c0c72b666b
Mars Sun <marsunique@gmail.com>
2016-12-14 04:26:58 -0500

    longest substring with at most k distinct char

---
+++ LongestSubstringWithAtMostKDistinctChar.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def lengthOfLongestSubstringKDistinct(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        if k == 0:
+            return 0
+        dic = {}
+        res = 0
+        end = 0
+        # add s[end] into the dict, remove one s[start] from dict and start++
+        for start in range(len(s)):
+            while end < len(s):
+                print start, s[start], dic, end, s[end]
+                if start != end:
+                    if s[end] in dic:
+                        dic[s[end]] += 1
+                        end += 1
+                    else:
+                        if len(dic) < k:
+                            dic.setdefault(s[end], 0)
+                            dic[s[end]] += 1
+                            end += 1
+                        else:
+                            dic[s[start]] -= 1
+                            if dic[s[start]] == 0:
+                                del(dic[s[start]])
+                            break
+                else:
+                    dic.setdefault(s[end], 0)
+                    dic[s[end]] += 1
+                    end += 1
+            res = max(res, end-start)
+        return res
+
+test = Solution()
+print test.lengthOfLongestSubstringKDistinct('abaccc',2)
