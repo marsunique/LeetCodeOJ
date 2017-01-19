e7cdd26c36a7a4e84c108bb90f5c138539117541
Mars Sun <marsunique@gmail.com>
2017-01-19 01:55:27 -0500

    reverse integer

---
+++ ReverseInteger.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        MAX = 0x7fffffff
+        MIN = ~(0x80000000 ^ 0xffffffff)
+        # use MAX and MIN to do overflow detection
+        # python uses more than 32 bits to indicate a number
+        sign = cmp(x, 0)
+        # get the sign of x
+        x = str(sign*x)[::-1]
+        x = int(x)*sign
+        if MIN <= x and x <= MAX:
+            return x
+        return 0
+        
+
+test = Solution()
+print test.reverse(-230)
