42f3fc18b3352bfe2315b9c5e84e10a4678e3b2e
Mars Sun <marsunique@gmail.com>
2017-06-18 12:03:04 -0400

    single numbers

--- SingleNumber.py
+++ SingleNumber.py
@@ -1,9 +1,15 @@
 class Solution(object):
     def singleNumber(self, nums):
-        result = 0
-        for num in nums:
-            result = result ^ num
-        return result
-num = Solution()
-array = [1,2,3,4,3,2,1,4,-2,-2,-1,-1,-0,0,100032]
-print num.singleNumber(array)
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in range(1, len(nums)):
+            nums[0] = nums[0] ^ nums[i]
+        return nums[0]
+            
+
+nums = [1,2,3,4,3,2,1,4,-2,-2,-1,-1,-0,0,100032]
+test = Solution()
+print test.singleNumber(nums)
+
