5c5885fb6ca0918e0d913c2e47ab69097f05cb39
Mars Sun <marsunique@gmail.com>
2017-06-18 11:23:44 -0400

    tow sum

--- TwoSum.py
+++ TwoSum.py
@@ -13,3 +13,20 @@ class Solution(object):
                 return [dic[nums[i]], i]
             else:
                 dic[target - nums[i]] = i 
+
+class Solution2(object):
+    def twoSum(self, nums, target):
+        if len(nums) < 2:
+            return False
+        else:
+            dic = {}
+            for i in range(0, len(nums)):
+                if target-nums[i] in dic:
+                    return [dic[target-nums[i]], i]
+                else:
+                    dic[nums[i]] = i
+
+nums = [2, 7, 11, 15]
+target = 9
+test = Solution2()
+print test.twoSum(nums, target)
