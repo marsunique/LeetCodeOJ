3084536fb5893e56892d7d0d28eafd634e456cbc
Mars Sun <marsunique@gmail.com>
2017-09-10 21:13:21 -0400

    complete kraken

--- UniquePaths.py
+++ UniquePaths.py
@@ -15,6 +15,22 @@ class Solution(object):
                     grid[row][col] = grid[row-1][col] + grid[row][col-1]
         return grid[m-1][n-1]
 
-
 test = Solution()
 print test.uniquePaths(4,4)
+
+# Variant of LC62-Unique Paths
+# Consider diagonal
+class Solution(object):
+    def completeKraken(self, m, n):
+        # grid = [[None] * n for i in range(m)]  //this works only because object None is immutable.
+        grid = [[None for j in range(n)] for i in range(m)]
+        for row in range(m):
+            for col in range(n):
+                if row == 0 or col == 0:
+                    grid[row][col] = 1
+                else:
+                    grid[row][col] = grid[row-1][col] + grid[row][col-1] + grid[row-1][col-1]
+        return grid[m-1][n-1]
+
+test = Solution()
+print test.completeKraken(3,3)
