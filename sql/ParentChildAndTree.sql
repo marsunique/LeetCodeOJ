a3a06e329a52b37ebff59427fd535851978acc5f
Mars Sun <marsunique@gmail.com>
2017-09-12 20:29:22 -0400

    parent children tree

--- sql/ParentChildAndTree.sql
+++ sql/ParentChildAndTree.sql
@@ -1,8 +1,20 @@
 SELECT TREE.ID,
     CASE
-        WHEN TREE.ID IS NULL THEN 'ROOT'
+        WHEN TREE.P_ID IS NULL THEN 'ROOT'
         WHEN EXISTS (SELECT T.P_ID FROM TREE AS T WHERE T.P_ID = TREE.ID) THEN 'INNER'
         ELSE 'LEAF'
     END
 FROM TREE
 ORDER BY TREE.ID;
+
+
+# Leetcode
+# Write your MySQL query statement below
+SELECT id,
+    CASE
+        WHEN p_id IS null THEN 'Root'
+        WHEN tree.id IN (SELECT t.p_id FROM tree AS t) THEN 'Inner'
+        ELSE 'Leaf'
+    END as Type
+FROM tree
+ORDER BY tree.id;
