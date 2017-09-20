171ac41abfa1d31a292c7c6e981c94fe3aa8a676
Mars Sun <marsunique@gmail.com>
2017-09-20 16:47:48 -0400

    minor change

--- MaximumSubarray.py
+++ MaximumSubarray.py
@@ -6,12 +6,12 @@ class Solution(object):
         """
         sum = 0
         res = float('-inf')
-        for i in range(len(nums)):
-            sum += nums[i]
+        for num in nums:
+            sum += num
             res = max(sum, res)
             if sum < 0:
                 sum = 0
         return res
-
+    
 test = Solution()
 print test.maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
