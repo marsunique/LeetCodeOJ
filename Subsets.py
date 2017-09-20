816967708aaec54900c166d56d571fe6ddcc1221
Mars Sun <marsunique@gmail.com>
2017-09-19 22:49:17 -0400

    subset

--- Subsets.py
+++ Subsets.py
@@ -4,10 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        # res = [[]]
+        # for num in nums:
+        #     temp = [[],[num]]
+        #     res = [sub+s for sub in res for s in temp]
+        # return res
+
         res = [[]]
         for num in nums:
-            temp = [[],[num]]
-            res = [sub+s for sub in res for s in temp]
+            temp = [[], [num]]
+            r = []
+            for candidate in temp:
+                for sub in res:
+                    r.append(sub+candidate)
+            res = r
         return res
+
+        
+
 test = Solution()
 print test.subsets([1,2,3,4])
