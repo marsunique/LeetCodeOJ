078a9169bdaf13c50ea1ebe0d46d3afe0b0e97b1
Mars Sun <marsunique@gmail.com>
2017-10-31 13:05:22 -0400

    modify fraction addition and subtraction

--- FractionAdditionAndSubtraction.py
+++ FractionAdditionAndSubtraction.py
@@ -4,27 +4,23 @@ class Solution(object):
         :type expression: str
         :rtype: str
         """
-        # use fractions to store numerators and denomenator with sign on numer
-        # fraction = [(numer, denom)]
-        fractions = []
+        # use fraction to store numerators and denomenator with sign on numer
+        # fraction = (numer, denom)
         sign = 1
         start = 0
+        subtotal = (0,1)
         for i, ch in enumerate(expression):
             if ch == '+' or ch == '-':
                 fraction = expression[start:i]
                 if fraction:
-                    fractions.append((sign*int(fraction.split('/')[0]), int(fraction.split('/')[1])))
+                    subtotal = self.add(subtotal, (sign*int(fraction.split('/')[0]), int(fraction.split('/')[1])))
                 if ch == '+':
                     sign = 1
                 else:
                     sign = -1
                 start = i + 1
         fraction = expression[start:]
-        fractions.append((sign*int(fraction.split('/')[0]), int(fraction.split('/')[1])))
-        # print fractions
-        subtotal = (0,1)
-        for fraction in fractions:
-            subtotal = self.add(subtotal, fraction)
+        subtotal = self.add(subtotal, (sign*int(fraction.split('/')[0]), int(fraction.split('/')[1])))
         gcd = self.gcd(subtotal[0], subtotal[1])
         return str(subtotal[0]/gcd) + '/' + str(subtotal[1]/gcd)
         
@@ -32,13 +28,12 @@ class Solution(object):
         numer = f1[0]*f2[1] + f1[1]*f2[0]
         denom = f1[1]*f2[1]
         return (numer, denom)
-    
+
     def gcd(self, a, b):
         if not a % b:
             return b
         else:
             return self.gcd(b, a%b)
-
 
 test = Solution()
 print test.fractionAddition('-1/2+1/2+1/3')
