1d6b83161790984bd862ecde04cb63e30c76227d
Mars Sun <marsunique@gmail.com>
2017-11-01 19:06:01 -0400

    modify unique path

--- UniquePaths.py
+++ UniquePaths.py
@@ -1,3 +1,28 @@
+# use 1d array
+# traverse by row, for current node [m][n], 
+# grid[n-1] is [m][n-1], grid[n] is [m-1][n],
+# update grid[n] by adding grid[n-1] and grid[n]
+
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        grid = [None] * n
+        for row in range(m):
+            for col in range(n):
+                if row == 0 or col == 0:
+                    grid[col] = 1
+                else:
+                    grid[col] = grid[col-1] + grid[col]
+        return grid[col]
+
+test = Solution()
+print test.uniquePaths(4,4)
+
+# use 2d array
 class Solution(object):
     def uniquePaths(self, m, n):
         """
