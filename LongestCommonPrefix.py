cb7d4801e9191babd2c4ad81bccba4da80109ba4
Mars Sun <marsunique@gmail.com>
2017-11-02 12:59:11 -0400

    change longest common prefix

--- LongestCommonPrefix.py
+++ LongestCommonPrefix.py
@@ -4,18 +4,17 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        if not strs:
-            return ''
         res = ''
+        if not strs:
+            return res
         for i in range(len(strs[0])):
-            letter = strs[0][i]
-            for string in strs:
-                if i < len(string) and letter == string[i]:
-                    continue
-                else:
-                    return res
-            res += letter
-        return res
+            for j in range(1, len(strs)):
+                if i >= len(strs[j]) or strs[0][i] != strs[j][i]:
+                    return strs[0][0:i]
+        return strs[0]
+
+'''time complexity is O(mn), m is the shortest length among strs,'''
+'''n is the number of strs'''
 
 test = Solution()
 print test.longestCommonPrefix([])
