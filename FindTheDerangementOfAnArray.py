982b9469a73494de8130d2964ab4677f8e124266
Mars Sun <marsunique@gmail.com>
2017-11-02 17:37:10 -0400

    minor change

--- FindTheDerangementOfAnArray.py
+++ FindTheDerangementOfAnArray.py
@@ -7,14 +7,14 @@ class Solution(object):
         # Reference: https://en.wikipedia.org/wiki/Derangement#Counting_derangements
         # The recursion relationship is D(n) = n-1*( D(n-1) + D(n-2) )
         MOD = 10**9+7
-        x2 = 1
-        x1 = 0
-        # when n = 1, doesn't enter loop return x1 = D(1)
-        # when n = 2, x1 = D(2), x2 = D(3), return x1 = D(2)
-        # when n = 3, x1 = D(3), x2 = D(4), return x1 = D(3) 
-        for i in range(2, n+1):
-            x1, x2 = x2, i*(x2+x1) % MOD
-        return x1
+        d2 = 1
+        d1 = 0
+        # when n = 1, doesn't enter loop return d1 = D(1)
+        # when n = 2, d1 = D(2), d2 = D(3), return d1 = D(2)
+        # when n = 3, d1 = D(3), d2 = D(4), return d1 = D(3) 
+        for i in range(3, n+2):
+            d1, d2 = d2, (i-1)*(d2+d1) % MOD
+        return d1
 
 test = Solution()
 print test.findDerangement(4)
