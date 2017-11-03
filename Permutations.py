b5773b607bd3d2c35d0a4c8fcab8eafd75a89a6c
Mars Sun <marsunique@gmail.com>
2017-11-03 03:17:37 -0400

    add derangement permutation

--- Permutations.py
+++ Permutations.py
@@ -19,3 +19,29 @@ class Solution(object):
 
 test = Solution()
 print test.permute([1,2,3])
+
+# only produce derangement permutation
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        original = nums[:]  # add original to compare
+        self.helper([], nums, res, original)
+        return res
+    
+    def helper(self, prefix, nums, res, original):
+        if not nums:
+            res.append(prefix)
+        else:
+            for i in range(len(nums)):
+                num = nums.pop(i)
+                new = prefix+[num]
+                if new[-1] != original[len(new)-1]:     # only derangement
+                    self.helper(prefix+[num], nums, res, original)
+                nums.insert(i, num)
+
+test = Solution()
+print test.permute([1,2,3])
