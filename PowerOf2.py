24b447d98f8d0f16f6550b6abfbcdd702032c551
Mars Sun <marsunique@gmail.com>
2017-11-28 02:41:11 -0500

    modify power of two

--- PowerOf2.py
+++ PowerOf2.py
@@ -1,16 +1,9 @@
 # Check whether a number is power of 2.
 # use bitwise operation
+# If n is power of 2, n-1 will 0111...111
 def powerOfTwo(n):
-    tester = 1
-    origin = n
-    while True:
-        if (n & 1):
-            if tester == origin:
-                return True
-            else:
-                return False
-        else:
-            n = n >> 1
-            tester = tester << 1
+    if n <= 0:
+        return False
+    return not (n & n-1)
 
 print powerOfTwo(4096)
