140e93449d0dde6db85204eaf7d786b7698bddb7
Mars Sun <marsunique@gmail.com>
2017-11-28 22:27:47 -0500

    fix bug basic calculator II

---
+++ BasicCalculatorII.py
@@ -0,0 +1,63 @@
+# LC 227
+# Have +, -, *, /, no parenthesis
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s.replace(' ', '')
+        s += '#'
+        opnd_stack =[]
+        optr_stack = []
+        priority = {
+            ('+','+'): True, ('+','-'): True, ('+','*'): False, ('+','/'): False,
+            ('-','+'): True, ('-','-'): True, ('-','*'): False, ('-','/'): False,
+            ('*','+'): True, ('*','-'): True, ('*','*'): True, ('*','/'): True,
+            ('/','+'): True, ('/','-'): True, ('/','*'): True, ('/','/'): True
+        }
+        num = 0
+        for ch in s:
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+            else:
+                opnd_stack.append(num)
+                num = 0
+                if ch == '#':
+                    break
+                    
+                # if optr_stack is empty, append
+                if not optr_stack:
+                    optr_stack.append(ch)
+
+                # compare priority
+                else:
+                    for i in range(len(optr_stack)):
+                        optr = optr_stack[-1]
+                        # optr > ch, calculate optr
+                        if priority[(optr, ch)]:
+                            self.doit(opnd_stack, optr_stack)
+                        else:
+                            break
+                    optr_stack.append(ch)
+
+        while optr_stack:
+            self.doit(opnd_stack, optr_stack)
+        return opnd_stack[0]
+    def doit(self, opnd_stack, optr_stack):
+        num2 = opnd_stack.pop()
+        num1 = opnd_stack.pop()
+        optr = optr_stack.pop()
+        if optr == '+':
+            num1 += num2
+        elif optr == '-':
+            num1 -= num2
+        elif optr == '*':
+            num1 *= num2
+        else:
+            num1 /= num2
+        opnd_stack.append(num1)
+        
+test = Solution()
+print test.calculate('11-6+2*3+50/10*2+12')
+print test.calculate("1*2-3/4+5*6-7*8+9/10")
